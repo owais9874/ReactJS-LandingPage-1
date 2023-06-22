1a8dc7351e116835efce816383edde109d239360
Owais <mowais9874@gmail.com>
2023-06-22 17:05:48 +0500

    Update

--- src/MyComponents/VideosSection/Videos.js
+++ src/MyComponents/VideosSection/Videos.js
@@ -1,15 +1,10 @@
-import React from 'react'
-import './Videos.css'
+import React from "react";
+import "./Videos.css";
 
 export default function Videos(props) {
   return (
-    // <div className="row">
-    //     <div className="col-md-12 col-12">
-            <div id='video' className='videomp4'>
-                <video autoPlay muted loop src={props.videoSection}
-                />
-            </div>
-    //     </div>
-    // </div>
-  )
+    <div id="video" className="videomp4">
+      <video autoPlay muted loop src={props.videoSection} />
+    </div>
+  );
 }
