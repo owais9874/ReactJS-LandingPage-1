bbd2f8f904ccd831ade9d0b5eb43a8e8e111dae0
Owais <mowais9874@gmail.com>
2023-06-23 13:10:22 +0500

    Update

---
+++ src/Data/testimonialData.js
@@ -0,0 +1,9 @@
+import img1 from "../assist/images/user1.png";
+import img2 from "../assist/images/user2.png";
+import img3 from "../assist/images/user3.png";
+
+export const testimonialData = {
+    image1: img1,
+    image2: img2,
+    image3: img3,
+}
--- src/MyComponents/VideosSection/Videos.js
+++ src/MyComponents/VideosSection/Videos.js
@@ -1,11 +1,10 @@
 import React from "react";
 import video from "../../assist/video/video.mp4"
-import "./Videos.css";
 
 export default function Videos() {
   return (
     <div id="video" className="videomp4">
-      <video autoPlay muted loop src={video} />
+      <video autoPlay muted loop width="100%" src={video} />
     </div>
   );
 }
