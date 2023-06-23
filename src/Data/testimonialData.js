e0abf3a6932ebece0056f0b1e9dfa43cfc0765f4
Owais <mowais9874@gmail.com>
2023-06-23 15:21:28 +0500

    update

--- src/Data/testimonialData.js
+++ src/Data/testimonialData.js
@@ -2,13 +2,30 @@ import img1 from "../assist/images/user1.png";
 import img2 from "../assist/images/user2.png";
 import img3 from "../assist/images/user3.png";
 
-export const testimonialData = {
-  //Testimonial1:
-  text1: ` "I work with Abdullah on many projects, he always exceeds my expectations with his quality work and fast service, very smooth and simple communication. Keep up the great work." `,
-  name1: "Leslie Alexander",
-  work1: "Photographer",
-  image1: img1,
-
-  image2: img2,
-  image3: img3,
-};
+export const testimonialData = [
+  {
+    //Testimonial1:
+    id: 1,
+    text: ` "I work with Abdullah on many projects, he always exceeds my expectations with his quality work and fast service, very smooth and simple communication. Keep up the great work." `,
+    name: "Leslie Alexander",
+    title: "Photographer",
+    image: img1
+  },
+  {
+    //Testimonial2:
+    id: 2,
+    text: ` "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content." `,
+    name: "Lorem ipsum",
+    title: "Photographer",
+    image: img2
+  },
+  {
+    //Testimonial3:
+    id: 3,
+    text: ` "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. " `,
+    name: "Alexander",
+    title: "Photographer",
+    image: img3
+  },
+  
+]
--- src/MyComponents/Testimonial/Testimonial.js
+++ src/MyComponents/Testimonial/Testimonial.js
@@ -1,55 +1,49 @@
-import React from 'react'
-import './Testimonial.css'
-import { testimonialData } from '../../Data/testimonialData'
+import React from "react";
+import "./Testimonial.css";
+import { testimonialData } from "../../Data/testimonialData";
+
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Testimonial() {
-    return (
-        <div id='testimonial' className="testmonial-sec">
-            <div className="testimonial-heading">TESTIMONIAL</div>
-            <div className="owl-carousel owl-carousel-testimonial">
-                <div className="item">
-                    <div className="testimonial-txt">
-                        {testimonialData.text1}
-                    </div>
-                    <div className='testmonial-user'>
-                        <div className="testi-user-img">
-                            <img src={testimonialData.image1} alt=''/>
-                        </div>
-                        <div className="testi-user-name">
-                            <h5>{testimonialData.name1}</h5>
-                            <span>{testimonialData.work1}</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="testimonial-txt">
-                        "I work with Abdullah on many projects, he always exceeds my expectations with his quality work and fast service, very smooth and simple communication. Keep up the great work."
-                    </div>
-                    <div className='testmonial-user'>
-                        <div className="testi-user-img">
-                            <img src={testimonialData.image2} alt=''/>
-                        </div>
-                        <div className="testi-user-name">
-                            <h5>Leslie Alexander</h5>
-                            <span>Photographer</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="testimonial-txt">
-                        "I work with Abdullah on many projects, he always exceeds my expectations with his quality work and fast service, very smooth and simple communication. Keep up the great work."
-                    </div>
-                    <div className='testmonial-user'>
-                        <div className="testi-user-img">
-                            <img src={testimonialData.image3} alt=''/>
-                        </div>
-                        <div className="testi-user-name">
-                            <h5>Leslie Alexander</h5>
-                            <span>Photographer</span>
-                        </div>
-                    </div>
-                </div>
+    
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    loop: true,
+    autoplaySpeed: 3000,
+    draggable: true,
+    swipeToSlide: true,
+    swipe: true,
+  };
+
+  return (
+    <div id="testimonial" className="testmonial-sec">
+      <div className="testimonial-heading">TESTIMONIAL</div>
+
+      <Slider {...settings}>
+        {testimonialData.map((test) => (
+          <div className="single--testimony" key={test.id}>
+            <div className="testimonial-txt">{test.text}</div>
+            <div className="testmonial-user">
+              <div className="testi-user-img">
+                <img src={test.image} alt="" />
+              </div>
+              <div className="testi-user-name">
+                <h5>{test.name}</h5>
+                <span>{test.title}</span>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
