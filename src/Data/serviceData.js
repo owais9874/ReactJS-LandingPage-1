ca30f28bb7e4be295f472681200987d8161a881d
Owais <mowais9874@gmail.com>
2023-06-26 14:37:03 +0500

    Update

---
+++ src/Data/serviceData.js
@@ -0,0 +1,62 @@
+import { BiCamera, BiCameraMovie, BiImage, BiVideo } from "react-icons/bi"
+import { BsAirplane, BsBadgeAd, BsBalloonHeart, BsCollectionPlay, } from "react-icons/bs"
+
+
+export const serviceData = [
+    {
+        //service 1
+        id: 1,
+        serIcon: <BiCamera/>,
+        servBtn: "Photography",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 2
+        id: 2,
+        serIcon: <BiVideo/>,
+        servBtn: "Videography",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 3
+        id: 3,
+        serIcon: <BiImage/>,
+        servBtn: "Fashion Photo",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 4
+        id: 4,
+        serIcon: <BsBalloonHeart/>,
+        servBtn: "Wedding Photo",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 5
+        id: 5,
+        serIcon: <BiCameraMovie/>,
+        servBtn: "Filming",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 6
+        id: 6,
+        serIcon: <BsBadgeAd/>,
+        servBtn: "Advertising",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 7
+        id: 7,
+        serIcon: <BsCollectionPlay/>,
+        servBtn: "Sports Videography",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+    {
+        //service 8
+        id: 8,
+        serIcon: <BsAirplane/>,
+        servBtn: "Travel Photo",
+        servText: "Your company needs more photography than you might think. We photograph your products.",
+    },
+]
--- src/MyComponents/Services/Service.js
+++ src/MyComponents/Services/Service.js
@@ -1,83 +1,32 @@
-import React from 'react'
-import './Service.css'
+import React from "react";
+import "./Service.css";
+import { serviceData } from "../../Data/serviceData";
 
 export default function Service() {
-    return (
-        <>
-            <section id='service' className='service'>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 col-12">
-                            <div className="service-heading">OUR SERVICES</div>
-                        </div>
-                    </div>
-                    <div className="all-service">
-                        <div className="row align-items-center">
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-camera2"></i></div>
-                                    <button>Photography</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-camera-video"></i></div>
-                                    <button>Videography</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-person-rolodex"></i></div>
-                                    <button>Fashion Photo</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-balloon-heart"></i></div>
-                                    <button>Wedding Photo</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="service-row">
-                        <div className="row align-items-center">
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-camera-reels"></i></div>
-                                    <button>Filming</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-badge-ad"></i></div>
-                                    <button>Advertising</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-collection-play"></i></div>
-                                    <button>Sports Videography</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                            <div className="col-md-3 col-6">
-                                <div className="service1">
-                                    <div className='icon'><i className="bi bi-airplane"></i></div>
-                                    <button>Travel Photo</button>
-                                    <p>Your company needs more photography than you might think. We photograph your products.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section id="service" className="service">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-12">
+              <div className="service-heading">OUR SERVICES</div>
+            </div>
+          </div>
+          <div className="all-service">
+            <div className="row align-items-center">
+              {serviceData.slice(0,8).reverse().map((servTxt) => (
+                <div className="col-md-3 col-6">
+                  <div className="service1" key={servTxt.id}>
+                    <div className="icon">{servTxt.serIcon}</div>
+                    <button>{servTxt.servBtn}</button>
+                    <p>{servTxt.servText}</p>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
